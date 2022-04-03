acc079a0e0d8a5a9acf42405cf6e2f97f33c3bde
Saroj Rai <raisaroj360@gmail.com>
2022-04-03 18:37:38 +0545

    .env fix

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
       "scontent.fbir5-1.fna.fbcdn.net",
       "images.saymedia-content.com"
     ]
+  },
+  env: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID:  process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID    
   }
 };
 
