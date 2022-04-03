2edbbe3379a6ac1a3576d681368c4d06f098339b
Saroj Rai <raisaroj360@gmail.com>
2022-04-04 00:30:31 +0545

    courses ui update

--- pages/courses/index.js
+++ pages/courses/index.js
@@ -4,6 +4,7 @@ import Navbar from '../../components/Navbar.js';
 import styles from '../../styles/courses/Courses.module.css';
 import { BsFillArrowDownCircleFill } from 'react-icons/bs';
 import { AiFillDownCircle } from 'react-icons/ai';
+import { FiSearch } from 'react-icons/fi';
 import { Accordian } from '../../components/Accordian.js';
 
 import CourseCard from '../../components/course/CourseCard.js';
@@ -11,10 +12,12 @@ import TestimonialCard from '../../components/testimonial/TestimonialCard.js';
 import Collapsible from 'react-collapsible';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
-
 import Marginer from '../../components/utils/Marginer.js';
-
 import Footer from '../../components/footer/Footer.js';
+import { GrSort } from 'react-icons/gr';
+import { FaLayerGroup } from 'react-icons/fa';
+import { AiOutlineCalendar } from 'react-icons/ai';
+
 
 const Trigger = (props) => {
   return <div className={styles["course-level"]}>
@@ -42,82 +45,79 @@ export default function Courses(){
         <BsFillArrowDownCircleFill size={24}/>
         <Marginer vertical="20px"/>        
 
+        <div className={styles.tabNavBar}>
+          <div className={styles.searchBar}>
+            <FiSearch size={24}/>
+            <Marginer/>
+            <input name="" type="text" value="" className={styles.searchInput}/>
+          </div>
+          <ul className={styles.tabItems}>
+            <li className={styles.tabItemActive}> All </li>
+            <li className={styles.tabItem}> Beginner </li>
+            <li className={styles.tabItem}> Intermediate </li>
+            <li className={styles.tabItem}> Advance </li>            
+          </ul>
+          <div className={styles.extras}>
+            <div className={styles.smallIcon}>
+              Sort
+              <Marginer/>
+              <GrSort size={20}/> </div>
+          </div>
+        </div>
+
+        <Marginer vertical="40px"/>
+
         <div className={styles.levelCoursesContainer}>
 
-          <Collapsible trigger={ <Trigger title="Level 1 Courses"/>}>
-            <div className={styles["level-courses"]}>
-              <CourseCard title="Computer Operator" bgColor="#FFEFE4" contents={
-                [
-                  "Computer Advance Fundamentals",
-                  "Microsoft Office Training",
-                  "Image Manipulation Basics",
-                  "Internet & Web Fundamentals",
-                  "Lok Shewa Preparation"
-                ]}/>
-              <CourseCard title="Accounting Package" bgColor="#E4FFF0" contents={
-                [
-                  "Microsoft Office Training",
-                  "Tally Accounting",
-                  "Advance Microsoft Excel",
-                  "Internet & Web Official Workds Fundamentals"
-                ]
-              }/>
-            </div>          
-          </Collapsible>
-
-          <Collapsible trigger={ <Trigger title="Level 2 Courses"/>}>
-            <div className={styles["level-courses"]}>
-              <CourseCard title="Computer Operator" bgColor="#FFEFE4" contents={
-                [
-                  "Computer Advance Fundamentals",
-                  "Microsoft Office Training",
-                  "Image Manipulation Basics",
-                  "Internet & Web Fundamentals",
-                  "Lok Shewa Preparation"
-                ]}/>
-              <CourseCard title="Accounting Package" bgColor="#E4FFF0" contents={
-                [
-                  "Microsoft Office Training",
-                  "Tally Accounting",
-                  "Advance Microsoft Excel",
-                  "Internet & Web Official Workds Fundamentals"
-                ]
-              }/>
+          <div className={styles.courseCard}>
+            <img alt="" src="/george.jpg" className={styles.courseImg}/>
+            <div className={styles.courseDetails}>
+              <p className={styles.captionText}> Beginner </p>
+              <Marginer vertical="5px"/>
+              <p className={styles.titleText}> Computer Basics </p>
+              <Marginer vertical="50px"/>
+              {/* <p> Sit amet, mattis vulputate enim nulla aliquet porttitor lacus, luctus accumsan tortor posuere ac ut. Amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam ut? </p> */}
+              <div className={styles.courseTimings}>
+                <div className={styles.lessons}> <FaLayerGroup/> <Marginer/> 23 Lessons </div>
+                <div className={styles.lessons}> <AiOutlineCalendar/> <Marginer/> 3 Months </div>
+              </div>
             </div>
-          </Collapsible>
+          </div>
 
-
-          <Collapsible trigger={ <Trigger title="Level 3 Courses"/>}>
-            <div className={styles["level-courses"]}>
-              <CourseCard title="Computer Operator" bgColor="#FFEFE4" contents={
-                [
-                  "Computer Advance Fundamentals",
-                  "Microsoft Office Training",
-                  "Image Manipulation Basics",
-                  "Internet & Web Fundamentals",
-                  "Lok Shewa Preparation"
-                ]}/>
-              <CourseCard title="Accounting Package" bgColor="#E4FFF0" contents={
-                [
-                  "Microsoft Office Training",
-                  "Tally Accounting",
-                  "Advance Microsoft Excel",
-                  "Internet & Web Official Workds Fundamentals"
-                ]
-              }/>
+          <div className={styles.courseCard}>
+            <img alt="" src="/george.jpg" className={styles.courseImg}/>
+            <div className={styles.courseDetails}>
+              <p className={styles.captionText}> Beginner </p>
+              <Marginer vertical="5px"/>
+              <p className={styles.titleText}> Advance Basics </p>
+              <Marginer vertical="50px"/>
+              {/* <p> Sit amet, mattis vulputate enim nulla aliquet porttitor lacus, luctus accumsan tortor posuere ac ut. Amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam ut? </p> */}
+              <div className={styles.courseTimings}>
+                <div className={styles.lessons}> <FaLayerGroup/> <Marginer/> 23 Lessons </div>
+                <div className={styles.lessons}> <AiOutlineCalendar/> <Marginer/> 3 Months </div>
+              </div>
             </div>
+          </div>
 
-          </Collapsible>
+          <div className={styles.courseCard}>
+            <img alt="" src="/george.jpg" className={styles.courseImg}/>
+            <div className={styles.courseDetails}>
+              <p className={styles.captionText}> Beginner </p>
+              <Marginer vertical="5px"/>
+              <p className={styles.titleText}> Computer Operator </p>
+              <Marginer vertical="50px"/>
+              {/* <p> Sit amet, mattis vulputate enim nulla aliquet porttitor lacus, luctus accumsan tortor posuere ac ut. Amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam ut? </p> */}
+              <div className={styles.courseTimings}>
+                <div className={styles.lessons}> <FaLayerGroup/> <Marginer/> 23 Lessons </div>
+                <div className={styles.lessons}> <AiOutlineCalendar/> <Marginer/> 3 Months </div>
+              </div>
+            </div>
+          </div>
+          
 
         </div>    
 
         <Marginer vertical="30px"/>
-
-        {/* <TestimonialCard */}
-        {/*   content={"Viverra aliquet eget sit amet tellus. Sit amet, consectetur adipiscing elit pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas integer eget aliquet nibh praesent tristique."} */}
-        {/*   reviewer={"Ful Maya"} */}
-        {/* /> */}
-
 
       </main>
 
