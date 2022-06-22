8d4442d14d373ce18701090a0cfe24e06fa391cc
Saroj Rai <raisaroj360@gmail.com>
2022-06-22 23:18:41 +0545

    course page

--- pages/courses/[id].js
+++ pages/courses/[id].js
@@ -1,48 +1,62 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Navbar from '../../components/Navbar.js';
+import Head from "next/head";
+import Image from "next/image";
+import Navbar from "../../components/Navbar.js";
+import Footer from "../../components/footer/Footer.js";
 
-import { useRouter } from 'next/router';
-import { coursesList } from '../../components/course/coursesList.js';
-import PrimaryButton from '../../components/buttons/PrimaryButton.js';
+import { useRouter } from "next/router";
+import { coursesList } from "../../components/course/coursesList.js";
+import PrimaryButton from "../../components/buttons/PrimaryButton.js";
 
-import useAuth from '../../lib/hooks/Auth.js';
-import { UserService } from '../../lib/service/UserService.js';
-import styles from '../../styles/courses/courses_page.module.css';
+import useAuth from "../../lib/hooks/Auth.js";
+import { UserService } from "../../lib/service/UserService.js";
+import styles from "../../styles/courses/courses_page.module.css";
 
 export default function CoursePage(props) {
   const router = useRouter();
   const { id } = router.query;
   const course = coursesList.find((course) => course.id == id);
 
-  const {user, userData, registerWithEmailAndPassword} = useAuth();
+  const { user, userData, registerWithEmailAndPassword } = useAuth();
 
-  const CourseCoverImage = (props) => {
-  };
+  const CourseCoverImage = (props) => {};
 
   return (
     <div>
       <Head>
-        <title> Charicha Institute | Course | { course?.title } </title>
-        <meta name="description" content={"Charicha Institute - " + course?.title}/>
-        <link rel="icon" href="/favicon.ico" />        
+        <title> Charicha Institute | Course | {course?.title} </title>
+        <meta
+          name="description"
+          content={"Charicha Institute - " + course?.title}
+        />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <Navbar/>
+        <Navbar />
 
         <div styles={styles.heroContainer}>
-          <Image alt={"Computer Course - " + course?.title}
-                 src={course?.coverImg === undefined ? "/computer_course_cover_2.jpg" : course.coverImg }
-                 width="100%" height="30vh"
-                 layout="responsive" objectFit="cover"/>
-	  <div className={styles.containerDivider}>
+          <Image
+            alt={"Computer Course - " + course?.title}
+            src={
+              course?.coverImg === undefined
+                ? "/computer_course_cover_2.jpg"
+                : course.coverImg
+            }
+            width="100%"
+            height="30vh"
+            layout="responsive"
+            objectFit="cover"
+          />
+          <div className={styles.containerDivider}>
             <div styles={styles.heroContentContainer}>
               <p className={styles.courseTitle}> {course?.title} </p>
-              <p className={styles.courseDescription}> { course?.description } </p>
-              <PrimaryButton text="Start Learning"/>
+              <p className={styles.courseDescription}>
+                {course?.description}
+              </p>
+              <PrimaryButton text="Start Learning" />
             </div>
-            <div className={styles.heroContentContainer}>
+            <div className="heroContentContainer">
+              Welcome to the good night
             </div>
           </div>
         </div>
@@ -50,32 +64,33 @@ export default function CoursePage(props) {
         {/* <div className={styles.lessonsContainer}> */}
         {/* </div> */}
 
-	<div className={styles.detailsContainer}>
+        <div className={styles.detailsContainer}>
+          <div>
+            <p className={styles.headingTwo}> Lessons You&apos;ll learn </p>
+          </div>
 
-	<div>
-	  <p className={styles.headingTwo}> Lessons You&apos;ll learn </p>
+          <ol>
+            {course?.lessons?.map((lesson) => (
+              <li key={lesson}> {lesson} </li>
+            ))}
+          </ol>
+
+          {user != null ? (
+            <PrimaryButton
+              text="Enroll now"
+              onClick={async () => {
+                UserService.updateUser(user.uid, {
+                  [`courses.${course.id}`]: "",
+                });
+              }}
+            />
+          ) : (
+            <PrimaryButton text="Login to enroll now"></PrimaryButton>
+          )}
         </div>
-
-        <ol>
-          { course?.lessons?.map((lesson) => <li key={lesson}> {lesson} </li>)}
-        </ol>
-
-        { user != null ? 
-          <PrimaryButton text="Enroll now" onClick={async ()=> {
-            UserService.updateUser(user.uid, {
-              [`courses.${course.id}`]: ""
-            });
-        }}/> :
-          <h2>
-            Login to enroll now
-          </h2>
-        }
-
-        </div>    
-        
       </main>
 
+      <Footer />
     </div>
   );
-
 }
