b7e5767ae3f8007784bba87e62b66683a95a4a72
Saroj Rai <raisaroj360@gmail.com>
2023-01-01 21:52:37 +0000

    update: responsive landing page

--- components/home/AppStore.js
+++ components/home/AppStore.js
@@ -9,7 +9,7 @@ export default function AppStore(){
   };
 
   return (	<div className='w-full flex justify-center px-48'>
-	  <div className='flex justify-center gap-4'>
+	  <div className='flex flex-wrap justify-center gap-4'>
 	    <img className='max-w-xs' alt='Charicha Institute App' src='/app.png'/>
 	    <div className='max-w-lg flex flex-col justify-center'>
 	      <p className='text-3xl text-white'> Find us in PlayStore </p>
@@ -21,7 +21,7 @@ export default function AppStore(){
 	        <button className='w-full rounded-xl bg-cheeseyellow text-white shadow-lg' onClick={onSendClicked}> Send Link </button>
               </div>
 	      <p className='mt-2 text-xs text-white'> By clicking &quot;Send Link&quot; you agree to our Terms of Serivice & Privacy Policy.</p>
-              <a href='https://play.google.com/store/apps/dev?id=8427452924742673238&hl=en&gl=US&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
+              <a href='https:play.google.com/store/apps/dev?id=8427452924742673238&hl=en&gl=US&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https:play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
             </div>
 
           </div>
