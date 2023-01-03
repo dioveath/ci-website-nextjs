252d44662d1633c01b0e5bd135b1c07b89866be7
Saroj Rai <raisaroj360@gmail.com>
2023-01-03 15:51:44 +0000

    update: auth page ui update

--- pages/login/index.js
+++ pages/login/index.js
@@ -3,8 +3,6 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { useRef, useState } from 'react';
 
-import Navbar from '../../components/Navbar.js';
-
 import styles from '../../styles/login/login.module.css';
 import Marginer from '../../components/utils/Marginer.js';
 import PrimaryButton from '../../components/buttons/PrimaryButton.js';
@@ -14,13 +12,25 @@ import PuffLoader from 'react-spinners/PuffLoader';
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import { ImGoogle2 } from 'react-icons/im';
 
+import { useCallback } from 'react';
+import { loadFull } from 'tsparticles';
+import Particles from 'react-particles';
+import { particleConfig } from '../../lib/particle_config';
+
 export default function Login(){
   const email = useRef();
   const password = useRef();
 
   const { user, error, loading, loginWithGoogle, loginWithEmailAndPassword, logout } = useAuth();
-
   const [passwordShow, setPasswordShow] = useState(false);
+
+  const particlesInit = useCallback(async engine => {
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async container => {
+    console.log(container);
+  }, []);  
 
   const router = useRouter();
   if(user != null) {
@@ -39,15 +49,17 @@ export default function Login(){
         <meta name="description" content="Charicha Institute - Login Page" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        { !loading || user == null ?
+        <Particles init={particlesInit} loaded={particlesLoaded} options={particleConfig}/>
+      <main className={'flex w-full min-h-screen h-full justify-center items-center bg-gradient-[-45deg] from-eggblue to-slategray'}>
+        { loading &&  <div> <PuffLoader/> </div> }
+        { !loading &&
           <form className={styles.loginContainer} onSubmit={onSubmitHandler}>
-            <div className={'min-w-md w-[80%] h-full flex justify-center items-center'}>
-              <Image className="shadow-md" alt='charicha pc hero image' src='/ci_pc.svg' width={'700'} height={'500'}/>
+            <div className={'min-w-md w-full h-full flex justify-center items-center'}>
+              <Image className="shadow-md" alt='charicha pc hero image' src='/ci_pc.svg' width={'500'} height={'300'}/>
             </div>            
             {/* <img alt="" src="ci_pc.png" className={styles.image}/> */}
             <Marginer vertical="20px"/>
-            <p className={styles.captionStyle}> Login with credentials </p>
+            <p className={'text-sm text-white font-light'}> Login with credentials </p>
             <Marginer vertical="6px"/>          
             <input name="email" type="email" placeholder="Email" ref={email} className={styles.inputText}/>
             <Marginer vertical="14px"/>                    
@@ -69,20 +81,15 @@ export default function Login(){
               "alignItems": "center"
             }}> <ImGoogle2 size={26}/> <Marginer/>LOGIN WITH GOOGLE </div>}/>
             <Marginer vertical="14px"/>
-            <p className={styles.captionStyle} style={{ "cursor": "pointer" }}> Forget Your Password? </p>
+            <p className={'text-aquamarine font-light cursor-pointer'}> Forget Your Password? </p>
             <Marginer vertical="18px"/>
-            <div style={{
-              "display": "flex",
-              "gap": "5px"
-            }}>
-              <p className={styles.bodyTextStyle}> Don&apos;t have an Account? </p>
-              <p style={{
-                "cursor": "pointer"
-              }} onClick={() => {
+            <div className='flex gap-2'>
+              <p className={'text-white font-light'}> Don&apos;t have an Account? </p>
+              <p className={'text-aquamarine font-light cursor-pointer'} onClick={() => {
                 router.push('/register');
               }}> Register </p>
             </div>
-          </form> : <div> <PuffLoader/> </div>
+          </form> 
         }
       </main>
 
