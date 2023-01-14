8ccdbad06ac8b722d187eaf71de39326fc1fd437
Saroj Rai <raisaroj360@gmail.com>
2023-01-14 19:33:50 +0000

    fix: reacteditorjs fix

--- components/ReactEditorJS/index.js
+++ components/ReactEditorJS/index.js
@@ -3,24 +3,26 @@ import { createReactEditorJS } from "react-editor-js";
 import "../../styles/react-editor/react-editor.module.css";
 import { UserService } from "../../lib/service/UserService";
 
-const Table = dynamic(() => import ("@editorjs/table"), { ssr: false });
-const List = dynamic(() => import ("@editorjs/list"), { ssr: false });
-const Warning = dynamic(() => import("@editorjs/warning"), { ssr: false });
-const Code = dynamic(() => import("@editorjs/code"), { ssr: false });
-
-const LinkTool = dynamic(() => import("@editorjs/link"), { ssr: false });
-const Image = dynamic(() => import("@editorjs/image"), { ssr: false });
-const Header = dynamic(() => import("@editorjs/header"), { ssr: false });
-const Quote = dynamic(() => import("@editorjs/quote"), { ssr: false });
-const Marker = dynamic(() => import("@editorjs/marker"), { ssr: false });
-const CheckList = dynamic(() => import("@editorjs/checklist"), { ssr: false });
-const InlineCode = dynamic(() => import("@editorjs/inline-code"), { ssr: false });
-const SimpleImage = dynamic(() => import("@editorjs/simple-image"), { ssr: false});
-const Undo = dynamic(() => import('editorjs-undo'), { ssr: false });
-
-const Embed = dynamic(() => import('@editorjs/embed'), { ssr: false });
+import Embed from '@editorjs/embed';
+import Table from "@editorjs/table";
+import List from "@editorjs/list";
+import Warning  from "@editorjs/warning";
+import Code  from "@editorjs/code";
+import LinkTool  from "@editorjs/link";
+import Image  from "@editorjs/image";
+import Raw from '@editorjs/raw';
+import Header  from "@editorjs/header";
+import Quote  from "@editorjs/quote";
+import Marker  from "@editorjs/marker";
+import CheckList  from "@editorjs/checklist";
+import Delimiter from '@editorjs/delimiter';
+import InlineCode  from "@editorjs/inline-code";
+import SimpleImage  from "@editorjs/simple-image";
+import Undo  from 'editorjs-undo';
 
 const EditorJS = createReactEditorJS();
+
+console.log(Embed);
 
 const ReactEditorJS = ({ data, images, innerRef, userId }) => {
   const EDITOR_JS_TOOLS = {
@@ -48,12 +50,11 @@ const ReactEditorJS = ({ data, images, innerRef, userId }) => {
         },
       },
     },
-    // raw: Raw,
     header: Header,
     quote: Quote,
     marker: Marker,
     checklist: CheckList,
-    // delimiter: Delimiter,
+    delimiter: Delimiter,
     inlineCode: InlineCode,
     simpleImage: SimpleImage,
   };
@@ -64,7 +65,7 @@ const ReactEditorJS = ({ data, images, innerRef, userId }) => {
 
   const handleReady = () => {
     const editor = innerRef.current._editorJS;
-    new Undo({ editor });
+    // new Undo({ editor });
   };
 
   return (
